abcf91f9e879b6f0cfdda36cc34b45f033378926
KindaMe <kalinowski1999@gmail.com>
2023-11-14 09:53:54 +0100

    Init Commit.

---
+++ currency-exchange-maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace currency_exchange_maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ currency-exchange-maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace currency_exchange_maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
