10689f024c0d697e70776da2984363bb9b0b164a
KindaMe <kalinowski1999@gmail.com>
2023-11-14 11:05:08 +0100

    Add Wallet, Rates and Profile pages. - Fill Wallet page with placeholder content - Add custom colors to dictionary

---
+++ currency-exchange-maui/RatesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency_exchange_maui;
+
+public partial class RatesPage : ContentPage
+{
+    public RatesPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ currency-exchange-maui/WalletPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency_exchange_maui;
+
+public partial class WalletPage : ContentPage
+{
+    public ObservableCollection<TestContentModel> TestContent { get; set; }
+    
+    public WalletPage()
+    {
+        InitializeComponent();
+        
+        TestContent = new ObservableCollection<TestContentModel>();
+        
+        TestContent.Add(new TestContentModel("Test Wallet 1", 420.69, "USD"));
+        TestContent.Add(new TestContentModel("Test Wallet 2", 420.69, "GBP"));
+        TestContent.Add(new TestContentModel("Test Wallet 1", 420.69, "USD"));
+        TestContent.Add(new TestContentModel("Test Wallet 2", 420.69, "GBP"));
+        TestContent.Add(new TestContentModel("Test Wallet 1", 420.69, "USD"));
+        TestContent.Add(new TestContentModel("Test Wallet 2", 420.69, "GBP"));
+        TestContent.Add(new TestContentModel("Test Wallet 1", 420.69, "USD"));
+        TestContent.Add(new TestContentModel("Test Wallet 2", 420.69, "GBP"));
+        TestContent.Add(new TestContentModel("Test Wallet 1", 420.69, "USD"));
+        TestContent.Add(new TestContentModel("Test Wallet 2", 420.69, "GBP"));
+        TestContent.Add(new TestContentModel("Test Wallet 1", 420.69, "USD"));
+        TestContent.Add(new TestContentModel("Test Wallet 2", 420.69, "GBP"));
+        TestContent.Add(new TestContentModel("Test Wallet 1", 420.69, "USD"));
+        TestContent.Add(new TestContentModel("Test Wallet 2", 420.69, "GBP"));
+        TestContent.Add(new TestContentModel("Test Wallet 1", 420.69, "USD"));
+        TestContent.Add(new TestContentModel("Test Wallet 2", 420.69, "GBP"));
+
+        BindingContext = this;
+    }
+}
+
+public class TestContentModel
+{
+    public TestContentModel(string name, double balance, string currency)
+    {
+        this.Name = name;
+        this.Balance = balance;
+        this.Currency = currency;
+    }
+
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public string Currency { get; set; }
+}
